2e83cf73eb46779421823bb1b351841c29fb9135
Matt Buck <matt@voxable.io>
2020-05-20 15:36:58 -0500

    Fix tests

--- alexa.gemspec
+++ alexa.gemspec
@@ -21,5 +21,5 @@ Gem::Specification.new do |s|
 
   s.add_dependency "rails", ">= 5.1"
 
-  s.add_development_dependency "pg"
+  s.add_development_dependency "sqlite3"
 end
--- test/dummy/config/application.rb
+++ test/dummy/config/application.rb
@@ -3,7 +3,7 @@ require_relative 'boot'
 require 'rails/all'
 
 Bundler.require(*Rails.groups)
-require "alexa"
+require "alexa-rails"
 
 module Dummy
   class Application < Rails::Application
